740f8f7a0812a257d3976824eea03af884e87415
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2020-11-27 08:02:25 -0500

    Module creations

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule
+  ],
+  exports: [
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+    DashboardComponent,
+  ]
+})
+export class PagesModule { }
